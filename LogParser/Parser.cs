4cf8f18376693432c5b9b9faa62411fa3d33a96e
Randy Hale <rhale78@gmail.com>
2017-01-23 13:03:38 -0500

    Changes due to log files not always being in zips

--- LogParser/Parser.cs
+++ LogParser/Parser.cs
@@ -76,9 +76,13 @@ namespace LogParser
 		{
 			return true;
 		}
+		public bool ProcessLogFile(string file)
+		{
+			return true;
+		}
 		public bool ProcessLogEntry(string line)
 		{
-			if(HasLogHeader(line))
+			if (HasLogHeader(line))
 			{
 				DateTime logDate = GetLogHeaderDateTime(line);
 			}
@@ -89,93 +93,115 @@ namespace LogParser
 		{
 			try
 			{
-				if(System.IO.Directory.Exists("C:\\TmpLogParser"))
+				if (System.IO.Directory.Exists("C:\\TmpLogParser"))
 				{
 					DirectoryInfo info = new DirectoryInfo("C:\\TmpLogParser");
 					info.Delete(true);
 				}
 				System.IO.Directory.CreateDirectory("C:\\TmpLogParser");
 				string[] directories = System.IO.Directory.GetDirectories(StartPath);
-				foreach(string directory in directories)
+				foreach (string directory in directories)
 				{
-					string[] files = System.IO.Directory.GetFiles(directory, "*.zip");
-					foreach(string file in files)
+					ProcessZipFiles(directory);
+					ProcessLogFiles();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private void ProcessLogFiles()
+		{
+			string[] logFiles = System.IO.Directory.GetFiles("C:\\TmpLogParser");
+			foreach (string logFile in logFiles)
+			{
+				if (ProcessLogFile(logFile))
+				{
+					try
 					{
-						if (ProcessZipFile(file))
+						string[] lines = System.IO.File.ReadAllLines(logFile);
+						string logEntry = string.Empty;
+						bool validLog = false;
+						foreach (string currentLine in lines)
 						{
-							try
+							string line = currentLine;
+							if (!string.IsNullOrEmpty(line))
 							{
-								ZipArchive zipArchive = ZipFile.OpenRead(file);
-								foreach(ZipArchiveEntry entry in zipArchive.Entries)
+								if (!line.EndsWith(System.Environment.NewLine))
 								{
-									if(ProcessZipEntry(entry))
-									{
-										entry.ExtractToFile("C:\\TmpLogParser");
-									}
+									line = line + System.Environment.NewLine;
 								}
-								//ZipFile.ExtractToDirectory(file, "C:\\TmpLogParser");
-								string[] zipFiles = System.IO.Directory.GetFiles("C:\\TmpLogParser");
-								foreach (string zipFile in zipFiles)
+
+								if (HasLogHeader(line) && ProcessLogEntry(line))
 								{
-									string[] lines = System.IO.File.ReadAllLines(zipFile);
-									string logEntry = string.Empty;
-									bool validLog = false;
-									foreach (string currentLine in lines)
-									{
-										string line = currentLine;
-										if (!string.IsNullOrEmpty(line))
-										{
-											if (!line.EndsWith(System.Environment.NewLine))
-											{
-												line = line + System.Environment.NewLine;
-											}
-
-											if (HasLogHeader(line)&&ProcessLogEntry(line))
-											{
-												validLog = true;
-												if (!string.IsNullOrEmpty(logEntry))
-												{
-													WriteLog(logEntry);
-													logEntry = string.Empty;
-												}
-
-												logEntry = line;
-											}
-											else
-											{
-												if(HasLogHeader(line)&&!ProcessLogEntry(line))
-												{
-													validLog = false;
-												}
-												if (validLog)
-												{
-													logEntry = logEntry + line;
-												}
-											}
-										}
-									}
+									validLog = true;
 									if (!string.IsNullOrEmpty(logEntry))
 									{
 										WriteLog(logEntry);
 										logEntry = string.Empty;
 									}
-									if (System.IO.File.Exists(zipFile))
+
+									logEntry = line;
+								}
+								else
+								{
+									if (HasLogHeader(line) && !ProcessLogEntry(line))
 									{
-										System.IO.File.Delete(zipFile);
+										validLog = false;
+									}
+									if (validLog)
+									{
+										logEntry = logEntry + line;
 									}
 								}
 							}
-							catch (Exception ex)
+						}
+						if (!string.IsNullOrEmpty(logEntry))
+						{
+							WriteLog(logEntry);
+							logEntry = string.Empty;
+						}
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+				if (System.IO.File.Exists(logFile))
+				{
+					System.IO.File.Delete(logFile);
+				}
+			}
+		}
+
+		private void ProcessZipFiles(string directory)
+		{
+			string[] files = System.IO.Directory.GetFiles(directory, "*.zip");
+			if (files != null && files.Count() > 0)
+			{
+				foreach (string file in files)
+				{
+					if (ProcessZipFile(file))
+					{
+						try
+						{
+							ZipArchive zipArchive = ZipFile.OpenRead(file);
+							foreach (ZipArchiveEntry entry in zipArchive.Entries)
 							{
-								Console.WriteLine(ex.Message);
+								if (ProcessZipEntry(entry))
+								{
+									entry.ExtractToFile("C:\\TmpLogParser");
+								}
 							}
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex.Message);
 						}
 					}
 				}
-			}
-			catch(Exception ex)
-			{
-				Console.WriteLine(ex.Message);
 			}
 		}
 
